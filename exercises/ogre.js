f3278c9c15ccdf9ef8b8ed7501a961bb36cbcf9c
Ryan Hantak <rhantak@gmail.com>
2020-01-24 19:29:47 -0700

    Complete ogre

---
+++ exercises/ogre.js
@@ -0,0 +1,29 @@
+require('../exercises/human');
+
+class Ogre {
+  constructor(name, home = 'Swamp'){
+    this.name = name;
+    this.home = home;
+    this.swings = 0;
+  }
+
+  encounter(human){
+    human.encounterCounter++;
+    if(human.noticesOgre()){
+      this.swingAt(human)
+    }
+  }
+
+  swingAt(human){
+    this.swings ++;
+    if(this.swings % 2 === 0){
+      human.knockedOut = true;
+    }
+  }
+
+  apologize(human){
+    human.knockedOut = false;
+  }
+}
+
+module.exports = Ogre;
