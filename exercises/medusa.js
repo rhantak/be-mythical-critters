41d914c330d570f0c0e442ee5c3cd78b26f40e60
Ryan Hantak <rhantak@gmail.com>
2020-01-24 18:00:52 -0700

    Complete medusa

---
+++ exercises/medusa.js
@@ -0,0 +1,19 @@
+var Person = require('../exercises/person');
+
+class Medusa {
+  constructor(name){
+    this.name = name;
+    this.statues = [];
+  }
+
+  stare(victim){
+    this.statues.push(victim);
+    victim.stoned = true;
+    if(this.statues.length > 3){
+      this.statues[0].stoned = false
+      this.statues.shift();
+    }
+  }
+}
+
+module.exports = Medusa;
